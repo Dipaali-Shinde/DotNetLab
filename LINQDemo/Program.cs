7a01cc7167f65ab4d35465149c18b87da5b39f03
Dipali Shinde <dipalishinde264gmail.com>
2023-07-16 13:14:44 +0530

    1st program

---
+++ LINQDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace LINQDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            var sortedDays = daysOfWeek.OrderBy(day => Array.IndexOf(daysOfWeek, day));
+
+            Console.WriteLine("Days of the Week:");
+
+            foreach (var day in sortedDays)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
